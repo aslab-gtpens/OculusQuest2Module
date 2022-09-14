3fffac3ebe285f43f674d8e5437fac17d1e8f1d0
Atqa Munzir <atqamz@gmail.com>
2022-09-14 13:45:16 +0700

    init

---
+++ Assets/CubeSpawn.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+}
